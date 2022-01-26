ef85d199e94e7d1661ec455ac5ddfb6d95b45cb5
20monisha <86678290+20monisha@users.noreply.github.com>
2022-01-26 17:21:37 +0530

    Added initial bs website files

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomNumber1=Math.floor(Math.random()*6+1);
+var randomImage="dice"+randomNumber1+".png";
+var value="images/"+randomImage;
+var img1=document.querySelectorAll("img")[0];
+img1.setAttribute("src",value);
+
+var randomNumber2=Math.floor(Math.random()*6+1);
+var randomImage2="dice"+randomNumber2+".png";
+var value2="images/"+randomImage2;
+var img2=document.querySelectorAll("img")[1];
+img2.setAttribute("src",value2);
+
+if(randomNumber1>randomNumber2){
+  document.querySelector("h1").textContent="Player 1 Won";
+}else if(randomNumber1<randomNumber2){
+  document.querySelector("h1").textContent="Player 2 Won";
+}else{
+  document.querySelector("h1").textContent="Draw!!!";
+}
